3948e2cd61f8ca18e4d189db9aeb546843fc2770
minniexp <minyyang35@gmail.com>
2022-12-26 20:37:57 -0600

    testing env 2

--- src/App.js
+++ src/App.js
@@ -47,8 +47,8 @@ export default function NewApp() {
           method: 'GET',
           url: `https://deezerdevs-deezer.p.rapidapi.com/playlist/${playlistID}`,
           headers: {
-            'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}`,
-            'X-RapidAPI-Host': `${process.env.REACT_APP_API_HOST}`
+            'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+            'X-RapidAPI-Host': process.env.REACT_APP_API_HOST
           }
         }
         console.log(process.env.REACT_APP_API_HOST)
