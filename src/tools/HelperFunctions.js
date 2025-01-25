cebdf372af1c3fe77a61e1002fcc627019e20a21
minniexp <minyyang35@gmail.com>
2025-01-25 01:45:30 -0600

    randomizer function logging

--- src/tools/HelperFunctions.js
+++ src/tools/HelperFunctions.js
@@ -1,19 +1,19 @@
 const randomUnique = (range, count) => {
-    let nums = new Set();
-    while (nums.size < count) {
-        // Generate a number from 0 to range - 1
-        let randomNum = Math.floor(Math.random() * range);
-        nums.add(randomNum);
-    }
-    return [...nums];
+  let nums = new Set();
+  console.log("randomUnique: ", range, count);
+  while (nums.size < count) {
+    // Generate a number from 0 to range - 1
+    let randomNum = Math.floor(Math.random() * range);
+    nums.add(randomNum);
+    console.log("nums: ", nums);
+  }
+  return [...nums];
 };
-
 
 const randomUniqueQuestion = (range) => {
-    // Generate a number from 0 to range - 1
-    return Math.floor(Math.random() * range);
+  // Generate a number from 0 to range - 1
+  console.log("randomUniqueQuestion: ", Math.floor(Math.random() * range));
+  return Math.floor(Math.random() * range);
 };
 
-export {
-    randomUnique, randomUniqueQuestion
-}
+export { randomUnique, randomUniqueQuestion };
