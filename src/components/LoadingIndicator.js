7fcc04016b0001ca384c3a0d68d92af12630f41f
minniexp <minyyang35@gmail.com>
2025-01-25 10:19:12 -0600

    added loader message

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -1,29 +1,29 @@
 //loading imports
 import { usePromiseTracker } from "react-promise-tracker";
-import {TailSpin} from 'react-loader-spinner'
+import { TailSpin } from "react-loader-spinner";
 
-export default function LoadingIndicator(){
-    // Loading Symbol
-    const LoadingIndicator = props => {
-        const { promiseInProgress } = usePromiseTracker();
-        
-        return (
-            promiseInProgress && 
-            <div
-            style={{
-                width: "100%",
-                height: "100",
-                display: "flex",
-                justifyContent: "center",
-                    alignItems: "center"
-            }}
-            >
-                <TailSpin color="#ff930f" height="200" width="200" />
-            </div>
-        )
-    }
-    
-    return(
-        <LoadingIndicator/>
-    )
+export default function LoadingIndicator() {
+  // Loading Symbol
+  const LoadingIndicator = (props) => {
+    const { promiseInProgress } = usePromiseTracker();
+
+    return (
+      promiseInProgress && (
+        <div
+          style={{
+            width: "100%",
+            height: "100",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TailSpin color="#ff930f" height="200" width="200" />
+          <p>If loading takes more than 30 seconds, please refresh the page.</p>
+        </div>
+      )
+    );
+  };
+
+  return <LoadingIndicator />;
 }
